ff373afbc1a2d8d96043da92dee19213b1d79eaa
ttchef <ruro0.edit@gmail.com>
2026-03-22 18:58:59 +0100

    error structure

--- logger.h
+++ logger.h
@@ -9,22 +9,53 @@
 #error "Logger types not definined"
 #endif
 
+#ifndef LOGGER_LEVELS
+#error "Logger levels not definied"
+#endif
+
+#define LOGGER_COLOR_RED "\x1b[31m"
+#define LOGGER_COLOR_RESET "\x1b[0m"
+
 /* generate enum */
-#define _X(type, msg, ...) type,
+#define _X(level, type, msg, ...) level##_##type,
 enum { LOGGER_TYPES };
 #undef _X
 
-/* generate text */
-#define _X(type, msg, ...) msg,
-const char *LOGGER_NAMES[] = {LOGGER_TYPES};
-;
-#undef _X
-
-#define _X(type, msg, ...) +1
+#define _X(level, type, msg, ...) +1
 static const int LOGGER_ERRORS_LEN = 0 LOGGER_TYPES;
 #undef _X
 
-#define PRINT_ERROR(type) printf("[ERROR]: %s\n", LOGGER_NAMES[type])
+/* logger level enum */
+#define _X(name) name,
+enum { LOGGER_LEVELS };
+#undef _X
+
+/* logger level string */
+#define _X(name) #name,
+const char *LOGGER_LEVEL_NAMES[] = {LOGGER_LEVELS};
+#undef _X
+
+struct logger_entry {
+    int level;
+    const char *msg;
+};
+
+/* generate struct */
+#define _X(level, type, msg, ...) {level, msg},
+struct logger_entry LOGGER_TABLE[] = {LOGGER_TYPES};
+#undef _X
+
+static inline void logger_log(const char *file, const char *func, int line,
+                              struct logger_entry *e) {
+    const char *level = LOGGER_LEVEL_NAMES[e->level];
+    fprintf(stderr,
+            LOGGER_COLOR_RED "[%s] " LOGGER_COLOR_RESET "(%s:%d %s): %s\n",
+            level, file, line, func, e->msg);
+}
+
+#define PRINT_ERROR(type)                                                      \
+    logger_log(__FILE__, __func__, __LINE__, &LOGGER_TABLE[type])
+
 #define THROW_ERROR(type)                                                      \
     PRINT_ERROR(type);                                                         \
     exit(EXIT_FAILURE)
--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 
 #include <stdio.h>
 
+#define LOGGER_LEVELS                                                          \
+    _X(INFO)                                                                   \
+    _X(WARN)                                                                   \
+    _X(ERROR)
+
 #define LOGGER_TYPES                                                           \
-    _X(OUT_OF_MEMORY, "out of memory")                                         \
-    _X(NULL_PTR_DEREF, "null deref")
+    _X(ERROR, OUT_OF_MEMORY, "out of memory")                                  \
+    _X(ERROR, NULL_PTR_DEREF, "null deref")                                    \
+    _X(WARN, VALUE_NOT_SET, "value not set")
 
 #include "logger.h"
 
 int main() {
-    THROW_ERROR(OUT_OF_MEMORY);
+    THROW_ERROR(ERROR_OUT_OF_MEMORY);
     return 0;
 }
