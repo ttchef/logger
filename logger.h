a900bfff2f5771d9a3556b44eeca3ef63a1a15fb
ttchef <ruro0.edit@gmail.com>
2026-03-22 19:17:55 +0100

    selected colors for loggin types

--- logger.h
+++ logger.h
@@ -13,17 +13,24 @@
 #error "Logger levels not definied"
 #endif
 
-#define LOGGER_COLOR_BLACK "\x1b[30m"
-#define LOGGER_COLOR_RED "\x1b[31m"
-#define LOGGER_COLOR_GREEN "\x1b[32m"
-#define LOGGER_COLOR_YELLOW "\x1b[33m"
-#define LOGGER_COLOR_BLUE "\x1b[34m"
-#define LOGGER_COLOR_MAGENTA "\x1b[35m"
-#define LOGGER_COLOR_CYAN "\x1b[36m"
-#define LOGGER_COLOR_WHITE "\x1b[37m"
+#define LOGGER_COLORS                                                          \
+    _Y(LOGGER_COLOR_BLUE, "\x1b[34m")                                          \
+    _Y(LOGGER_COLOR_YELLOW, "\x1b[33m")                                        \
+    _Y(LOGGER_COLOR_RED, "\x1b[31m")                                           \
+    _Y(LOGGER_COLOR_GREEN, "\x1b[32m")
 
 #define LOGGER_COLOR_BOLD "\x1b[1m"
 #define LOGGER_COLOR_RESET "\x1b[0m"
+
+/* generate color enum */
+#define _Y(type, str) type,
+enum { LOGGER_COLORS };
+#undef _Y
+
+/* generate color str array */
+#define _Y(type, str) str,
+static const char *LOGGER_COLOR_STR[] = {LOGGER_COLORS};
+#undef _Y
 
 /* generate enum */
 #define _X(level, type, msg, ...) level##_##type,
@@ -35,13 +42,17 @@ static const int LOGGER_ERRORS_LEN = 0 LOGGER_TYPES;
 #undef _X
 
 /* logger level enum */
-#define _X(name) name,
+#define _X(name, color) name,
 enum { LOGGER_LEVELS };
 #undef _X
 
-/* logger level string */
-#define _X(name) #name,
-const char *LOGGER_LEVEL_NAMES[] = {LOGGER_LEVELS};
+struct logger_level {
+    int color;
+    const char *name;
+};
+
+#define _X(name, color) {color, #name},
+const struct logger_level LOGGER_LEVEL_TABLE[] = {LOGGER_LEVELS};
 #undef _X
 
 struct logger_entry {
@@ -54,24 +65,12 @@ struct logger_entry {
 struct logger_entry LOGGER_TABLE[] = {LOGGER_TYPES};
 #undef _X
 
-static inline const char *logger_level_to_color(int level) {
-    switch (level) {
-    case 0:
-        return LOGGER_COLOR_BLUE;
-    case 1:
-        return LOGGER_COLOR_YELLOW;
-    case 2:
-        return LOGGER_COLOR_RED;
-    default:
-        return LOGGER_COLOR_WHITE;
-    }
-}
-
 static inline void logger_log(const char *file, const char *func, int line,
                               struct logger_entry *e) {
-    const char *level = LOGGER_LEVEL_NAMES[e->level];
+    const char *level = LOGGER_LEVEL_TABLE[e->level].name;
     fprintf(stderr, "%s[%s] " LOGGER_COLOR_RESET "(%s:%d %s): %s\n",
-            logger_level_to_color(e->level), level, file, line, func, e->msg);
+            LOGGER_COLOR_STR[LOGGER_LEVEL_TABLE[e->level].color], level, file,
+            line, func, e->msg);
 }
 
 #define LOG(type) logger_log(__FILE__, __func__, __LINE__, &LOGGER_TABLE[type])
--- main.c
+++ main.c
@@ -2,19 +2,21 @@
 #include <stdio.h>
 
 #define LOGGER_LEVELS                                                          \
-    _X(INFO)                                                                   \
-    _X(WARN)                                                                   \
-    _X(ERROR)
+    _X(INFO, LOGGER_COLOR_BLUE)                                                \
+    _X(WARN, LOGGER_COLOR_YELLOW)                                              \
+    _X(ERROR, LOGGER_COLOR_RED)
 
 #define LOGGER_TYPES                                                           \
     _X(ERROR, OUT_OF_MEMORY, "out of memory")                                  \
     _X(ERROR, NULL_PTR_DEREF, "null deref")                                    \
-    _X(WARN, VALUE_NOT_SET, "value not set")
+    _X(WARN, VALUE_NOT_SET, "value not set")                                   \
+    _X(INFO, PRINTED, "printed smth")
 
 #include "logger.h"
 
 int main() {
     LOG(ERROR_OUT_OF_MEMORY);
     LOG(WARN_VALUE_NOT_SET);
+    LOG(INFO_PRINTED);
     return 0;
 }
